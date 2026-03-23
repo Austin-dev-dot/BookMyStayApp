d18c0bb1755a46530ce034992c6f20b85ea0aafa
Austin-dev-dot <austinpaulmail04@gmail.com>
2026-03-23 14:34:52 +0530

    Implement Use Case 2

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -16,7 +16,23 @@ public class BookMyStayApp {
         System.out.println("==================================================");
         System.out.println("Welcome to the Hotel Booking Management System!");
         System.out.println("Application Name: BookMyStayApp");
-        System.out.println("Version: v1.0");
+        System.out.println("Version: v2.0");
+        System.out.println("==================================================");
+
+        // UC2: Create room instances
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // UC2: Store availability using individual variables
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        System.out.println("\n--- Initializing Rooms and Static Availability ---");
+        System.out.println(singleRoom.toString() + " | Available: " + singleRoomAvailability);
+        System.out.println(doubleRoom.toString() + " | Available: " + doubleRoomAvailability);
+        System.out.println(suiteRoom.toString() + " | Available: " + suiteRoomAvailability);
         System.out.println("==================================================");
     }
 }
