f0b1769c258d93828f1a14f3c687d5ff735c0642
Austin-dev-dot <austinpaulmail04@gmail.com>
2026-03-23 15:26:27 +0530

    Implement Use Case 12

--- Reservation.java
+++ Reservation.java
@@ -1,4 +1,6 @@
-public class Reservation {
+import java.io.Serializable;
+
+public class Reservation implements Serializable {
     private String guestName;
     private Room requestedRoom;
     private String reservationId;
